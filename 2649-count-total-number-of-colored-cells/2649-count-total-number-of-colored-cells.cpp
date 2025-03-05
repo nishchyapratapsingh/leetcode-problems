6bbadfd6d67909b55ac1224d538855c96a01781c
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-03-05 21:57:00 +0530

    Time: 0 ms (100%), Space: 7.7 MB (91.36%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long sum=1;
+        for(int i=1; i<=n; i++) {
+            sum+=(i-1)*4;
+        }
+
+        return sum;
+    }
+};
