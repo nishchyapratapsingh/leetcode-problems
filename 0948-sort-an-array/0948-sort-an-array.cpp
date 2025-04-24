ba39bdfab991e692c9990b2565d019e5bd4b5d9a
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-04-24 21:07:35 +0530

    Time: 47 ms (73.39%), Space: 73.1 MB (63.07%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
 
-    void merge(vector<int>& nums, int s, int m, int e) {
+    void merge(vector<int>& nums, int s, int m, int e, vector<int>& temp) {
         int left = s;
+        int tempIdx = s;
         int right = m+1;
-        vector<int> temp;
-
         while (left<=m && right <=e) {
             if (nums[left]<nums[right]) {
-                temp.push_back(nums[left++]);
+                temp[tempIdx++]=nums[left++];
             }
             else {
-                temp.push_back(nums[right++]);
+                temp[tempIdx++]=nums[right++];
             }
         }
 
         while (left<=m) {
-            temp.push_back(nums[left++]);
+            temp[tempIdx++]=(nums[left++]);
         }
 
         while (right<=e) {
-            temp.push_back(nums[right++]);
+            temp[tempIdx++]=(nums[right++]);
         }
 
         for (int i=s; i<=e; i++) {
-            nums[i] = temp[i-s];
+            nums[i] = temp[i];
         }
     }
 
-    void mergeSort(vector<int>& nums, int s, int e) {
+    void mergeSort(vector<int>& nums, int s, int e, vector<int>& temp) {
         if (s>=e) {
             return;
         }
         int m=s+(e-s)/2;
-        mergeSort(nums, s, m);
-        mergeSort(nums, m+1, e);
-        merge(nums, s, m ,e);
+        mergeSort(nums, s, m, temp);
+        mergeSort(nums, m+1, e, temp);
+        merge(nums, s, m ,e, temp);
     }
 
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+        vector<int> temp(nums.size(), 0);
+        mergeSort(nums, 0, nums.size()-1, temp);
+
         return nums;
     }
 };
