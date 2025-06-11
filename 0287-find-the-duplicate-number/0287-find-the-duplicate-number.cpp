1db059313c21ad0c97339f50265254df384c8aba
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-06-11 23:03:10 +0530

    Time: 3 ms (68.49%), Space: 65 MB (69.13%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> freqMap;
-        for (auto i: nums) {
-            freqMap[i]++;
-            if (freqMap[i]>1) {
-                return i;
-            }
+        // Step 1: Find the intersection point of the two runners.
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        // Step 2: Find the entrance to the cycle (duplicate number)
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        
+        return slow;
     }
 };
