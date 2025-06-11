5a37a5dcb404e314d4015664ca29f7d6a353dde7
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-06-11 23:25:47 +0530

    Time: 3 ms (68.49%), Space: 65 MB (89.74%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // Step 1: Find the intersection point of the two runners.
+        //Floyd's Tortoise and Hare method
         int slow = nums[0];
         int fast = nums[0];
-        
+
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        } while (slow != fast);
-        
-        // Step 2: Find the entrance to the cycle (duplicate number)
-        slow = nums[0];
-        while (slow != fast) {
+        } while (slow!=fast);
+
+        fast = nums[0];
+        while (slow!=fast) {
             slow = nums[slow];
             fast = nums[fast];
         }
-        
-        return slow;
+        return fast;
     }
 };
