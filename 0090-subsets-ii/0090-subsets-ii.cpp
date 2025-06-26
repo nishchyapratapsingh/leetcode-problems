81ed00833f906e5c22ab697ac501fa222483a668
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-06-26 10:09:12 +0530

    Time: 3 ms (38.56%), Space: 10.9 MB (39.57%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void helper(set<vector<int>>& result, vector<int>& nums, int index, vector<int> currsubs) {
-        if (index == nums.size()) {
-            result.insert(currsubs);
-            return;
-        }
 
-        currsubs.push_back(nums[index]);
-        helper(result, nums, index+1, currsubs);
-        currsubs.pop_back();
-        helper(result, nums, index+1, currsubs);
+    void backtrack(vector<int>& nums, vector<vector<int>>& result, vector<int> currsum, int index) {
+        result.push_back(currsum);
+        for (int i=index; i<nums.size(); i++) {
+            if (i>index && nums[i]==nums[i-1]) continue;
+            currsum.push_back(nums[i]);
+            backtrack(nums, result, currsum, i+1);
+            currsum.pop_back();            
+        }
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        unordered_map<int, int> dupCheck;
-        set<vector<int>> result;
-        helper(result, nums, 0, {});
-        return vector<vector<int>>(result.begin(), result.end());
+        vector<vector<int>> result;
+
+        backtrack(nums, result, {}, 0);
+        return result;
     }
 };
