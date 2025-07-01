f77be5379268c3e3a8d6b49bb8b5b459d5957484
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-07-01 08:10:09 +0530

    Time: 3 ms (29.21%), Space: 28.2 MB (59.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        int maxfreq = 0;
-        int res = 0;
-        
-        for (int i=0; i<nums.size(); ++i) {
-            freq[nums[i]]++;
-            maxfreq = max(maxfreq, freq[nums[i]]);
-            if (maxfreq == freq[nums[i]]) res = nums[i];
+        //Boyer-Moore Voting
+        int count = 0;
+        int candidate = 0;
+        for (int num: nums) {
+            if (count==0) candidate = num;
+            count += num==candidate ? 1 : -1;
         }
 
-        return res;
+        return candidate;
     }
 };
