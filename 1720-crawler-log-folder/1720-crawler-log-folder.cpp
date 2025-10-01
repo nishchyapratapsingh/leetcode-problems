c0fb08b306f01e3ee9aa842428965d120dfa664a
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-10-01 21:58:16 +0530

    Time: 0 ms (100%), Space: 14.4 MB (28.66%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        stack<int> s;
+        stack<string> s;
         for (auto log: logs) {
             if (log=="../" && !s.empty()) {
                 s.pop();
             }
             else if (log!="./" && log!="../") {
-                s.push(1);
+                s.push(log);
             }
         }
         return s.size();
