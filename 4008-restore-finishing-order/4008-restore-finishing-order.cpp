75444f2e27e89ffe5dcb6afc7cf31a65a1458ba3
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-10-21 20:54:01 +0530

    Time: 14 ms (5.22%), Space: 52.6 MB (8.84%) - LeetHub

---
+++ 4008-restore-finishing-order/4008-restore-finishing-order.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        unordered_map<int, bool> id_table;
+        for (int id : friends) {
+            id_table[id] = true;
+        }
+        vector<int> ans;
+        for (int id : order) {
+            if (id_table[id] == true) {
+                ans.push_back(id);
+            }
+        }
+        return ans;
+    }
+};
