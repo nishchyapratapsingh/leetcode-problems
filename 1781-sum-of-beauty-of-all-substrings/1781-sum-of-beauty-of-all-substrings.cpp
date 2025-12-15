c17687ad9e075fd63744aa5172912f2efdaa45db
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-12-15 17:28:11 +0530

    Time: 121 ms (45.99%), Space: 9 MB (84.81%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -3,18 +3,16 @@ public:
     int beautySum(string s) {
         int n = s.length();
         int beauty = 0;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = i+1; j < n; j++) {
-                int freq[26] = {0};
-                for (int k = i; k <= j; k++) {
-                    freq[s[k]-'a']++;
-                }
+        for (int i = 0; i < n; i++) {
+            int freq[26] = {0};
+            for (int j = i; j < n; j++) {
+                freq[s[j]-'a']++;
                 int mini = INT_MAX;
                 int maxi = INT_MIN;
-                for (int l = 0; l < 26; l++) {
-                    if(freq[l] > 0) {
-                        mini = min(freq[l], mini);
-                        maxi = max(freq[l], maxi);
+                for (int k = 0; k < 26; k++) {
+                    if(freq[k] > 0) {
+                        mini = min(freq[k], mini);
+                        maxi = max(freq[k], maxi);
                     }
                 }
                 beauty += (maxi - mini);
