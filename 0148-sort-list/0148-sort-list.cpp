88bdb696d08d2360c53695d77079d7f38b2df270
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-12-18 23:38:48 +0530

    Time: 8 ms (90.69%), Space: 57 MB (89.72%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,8 +10,8 @@
  */
 class Solution {
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* tail = dummy;
+        ListNode dummy(-1); 
+        ListNode* tail = &dummy;
         while (list1 && list2) {
             if (list1 -> val < list2 -> val) {
                 tail -> next = list1;
@@ -30,9 +30,7 @@ class Solution {
         if (list2) {
             tail -> next = list2;
         }
-        ListNode* newNode = dummy->next;
-        delete dummy;
-        return newNode;
+        return dummy.next;
     }
     ListNode* middleList(ListNode* head) {
         ListNode* slow = head;
