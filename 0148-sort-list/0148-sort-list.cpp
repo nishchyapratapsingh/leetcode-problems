1ffa8bd3475c92260f83d9b891ada1d0d65ddb4a
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2025-12-18 23:34:34 +0530

    Time: 86 ms (5.11%), Space: 86.2 MB (5.52%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* tail = dummy;
+        while (list1 && list2) {
+            if (list1 -> val < list2 -> val) {
+                tail -> next = list1;
+                list1 = list1->next;
+                tail = tail->next;
+            }
+            else {
+                tail->next = list2;
+                list2 = list2->next;
+                tail = tail->next;
+            }
+        }
+        if (list1) {
+            tail -> next = list1;
+        }
+        if (list2) {
+            tail -> next = list2;
+        }
+        ListNode* newNode = dummy->next;
+        delete dummy;
+        return newNode;
+    }
+    ListNode* middleList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+        ListNode* mid = middleList(head);
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = NULL;
+        left = sortList(left);
+        right = sortList(right);
+        return mergeTwoLists(left, right);
+    }
+};
