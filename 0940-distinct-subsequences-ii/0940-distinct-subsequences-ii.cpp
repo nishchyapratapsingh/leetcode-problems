4a2700874c14780c3edb0f4f3b40ae0a65db71d3
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-02-05 21:24:52 +0530

    Time: 1 ms (50%), Space: 9.9 MB (18.39%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -5,16 +5,16 @@ public:
         long long mod = 1e9 + 7;
         vector<long long> t(n+1);
         t[0] = 1;
-        unordered_map<char, long long> last;
+        vector<int> last(26, -1);
 
         for (long long i = 1; i < n+1; i++) {
             t[i] = (t[i-1]*2)%mod;
 
-            if (last.find(s[i-1]) != last.end()) {
-                t[i] = ((t[i] - t[last[s[i-1]]])%mod + mod)%mod;
+            if (last[s[i-1] - 'a'] != -1) {
+                t[i] = ((t[i] - t[last[s[i-1] - 'a']])%mod + mod)%mod;
             }
             
-            last[s[i-1]] = i-1;
+            last[s[i-1] - 'a'] = i-1;
         }
 
         return (t[n]-1+mod)%mod;
