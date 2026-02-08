a4873d7e6cfbcdf1dff14ada44c5c8fc19468d17
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-02-09 00:11:29 +0530

    Time: 4 ms (59.97%), Space: 14.3 MB (69.85%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n = nums.size();
-		long long sum = 0;
-        int mod = 1e9 + 7;
-        for (int i = 0; i < n; i++) {
-        	long long mini = nums[i];
-        	long long maxi = mini;
-        	for (int j = i; j < n; j++) {
-        		mini = min((long long)nums[j], mini);
-        		maxi = max((long long)nums[j], maxi);
-
-        		sum = sum + maxi - mini;
+    long long subArrayRanges(vector<int>& arr) {
+    	int n = arr.size();
+    	stack<int> st;
+    	long long arrMins = 0;
+        for (int i = 0; i <= n; i++) {
+            int currElem = i == n ? INT_MIN : arr[i];
+        	while (!st.empty() && currElem < arr[st.top()]) {
+        		int top = st.top();
+        		st.pop();
+        		int pse = st.empty() ? -1 : st.top();
+        		int nse = i;
+        		long long leftContr = top - pse;
+        		long long rightContr = nse - top;
+        		arrMins = (arrMins + (((leftContr * rightContr)) * arr[top] ) );
         	}
+
+        	st.push(i);
         }
 
-        return sum;
+        stack<int> st2;
+
+        long long arrMaxs = 0;
+        for (int i = 0; i <= n; i++) {
+            int currElem = i == n ? INT_MAX : arr[i];
+        	while (!st2.empty() && currElem > arr[st2.top()]) {
+        		int top = st2.top();
+        		st2.pop();
+        		int pge = st2.empty() ? -1 : st2.top();
+        		int nge = i;
+        		long long leftContr = top - pge;
+        		long long rightContr = nge - top;
+        		arrMaxs = (arrMaxs + (((leftContr * rightContr)) * arr[top] ) );
+        	}
+
+        	st2.push(i);
+        }
+
+        return arrMaxs - arrMins;
     }
 };
