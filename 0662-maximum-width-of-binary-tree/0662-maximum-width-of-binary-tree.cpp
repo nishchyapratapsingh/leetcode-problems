3f15071d5f798ddf0343f4ee11b732f2e0e0ba45
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-03-05 21:55:11 +0530

    Time: 0 ms (100%), Space: 19.6 MB (94.59%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,34 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (!root) return 0;
-
-        queue<pair<TreeNode*, unsigned long long>> q;
-        q.push({root, 0});
-        int maxWidth = 0;
+        queue<pair<TreeNode*, long long>> q;
+        q.push({root, 1});
+        int maxi = 1;
 
         while (!q.empty()) {
             int size = q.size();
-            unsigned long long minIndex = q.front().second;
-            unsigned long long first, last;
+            long long first = q.front().second;
+            long long last = 0;
 
-            for (int i = 0; i < size; ++i) {
-                auto [node, index] = q.front();
+            for (int i = 0; i < size; i++)
+            {
+                auto temp = q.front();
                 q.pop();
-                index -= minIndex;
 
-                if (i == 0) first = index;
-                if (i == size - 1) last = index;
+                TreeNode* node = temp.first;
+                long long idx = temp.second;
+                idx-=first;
 
-                if (node->left)
-                    q.push({node->left, 2 * index});
-                if (node->right)
-                    q.push({node->right, 2 * index + 1});
+                if (i == size-1) last = idx;
+
+                if (node->left) {
+                    q.push({node->left, 2*idx});
+                }
+                if (node->right) {
+                    q.push({node->right, 2*idx+1});
+                }
             }
 
-            maxWidth = max(maxWidth, static_cast<int>(last - first + 1));
+            maxi = max(maxi, (int)last+1);
         }
 
-        return maxWidth;
+        return maxi;
     }
 };
