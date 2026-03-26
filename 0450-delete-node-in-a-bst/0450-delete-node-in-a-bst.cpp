54269621c7f3f87c296b1498285e09f3294096c2
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-03-26 10:04:53 +0530

    Time: 0 ms (100%), Space: 34.5 MB (14.45%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,73 +11,31 @@
  */
 class Solution {
     TreeNode* rightMost(TreeNode* root) {
-        if(!root) return root;
-        TreeNode* cur = root;
-        while (cur->right) {
-            cur = cur->right;
+        while (root->right) {
+            root = root->right;
         }
-        return cur;
+        return root;
     }
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if (!root) return root;
-        if (root->val == key) {
-            TreeNode* nd = root->left;
-            if (!nd) {
-                TreeNode* ans = root->right;
-                delete root;
-                return ans;
-            }
-            if (rightMost(nd)) rightMost(nd) ->right = root->right;
+        
+        if (root->val < key) {
+            root->right = deleteNode(root->right, key);
+        }
+        else if (root->val > key) {
+            root->left = deleteNode(root->left, key);
+        }
+        else {
+            if (!root->left) return root->right;
+            if (!root->right) return root->left;
+
+            TreeNode* rl = root->left;
+            TreeNode* rm = rightMost(rl);
+            rm->right = root->right;
             delete root;
-            return nd;
+            return rl;
         }
-
-        TreeNode* cur = root;
-        TreeNode* prev = cur;
-        while (cur && cur->val != key) {
-            prev = cur;
-            if (cur->val < key) {
-                cur = cur->right;
-            }
-            else {
-                cur = cur->left;
-            }
-        }
-
-        if (cur) {
-            if (prev->right == cur) {
-                if (!cur->left) {
-                    prev->right = cur->right;
-                    delete cur;
-                    return root;
-                }
-                if (!cur->right) {
-                    prev->right = cur->left;
-                    delete cur;
-                    return root;
-                }
-            }
-            else {
-                if (!cur->left) {
-                    prev->left = cur->right;
-                    delete cur;
-                    return root;
-                }
-                if (!cur->right) {
-                    prev->left = cur->left;
-                    delete cur;
-                    return root;
-                }
-            }
-
-            if (prev->left == cur) prev->left = cur->left;
-            else prev->right = cur->left;
-            if (rightMost(cur->left)) rightMost(cur->left)->right = cur->right;
-            delete cur;
-            return root;
-        }
-
         return root;
     }
 };
