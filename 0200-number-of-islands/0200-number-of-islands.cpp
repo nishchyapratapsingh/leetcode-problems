f3011f3a9d5104b1d76a20b6d5f76f9716371a90
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-05-02 15:56:52 +0530

    Time: 34 ms (17.29%), Space: 23.6 MB (23.68%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<bool>> vis(n, vector<bool> (m, 0));
+
+        vector<int> dr = {-1, 0, 1, 0};
+        vector<int> dc = {0, 1, 0, -1};
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] || grid[i][j] == '0') continue;
+
+                cnt++;
+                
+                queue<pair<int, int>> q;
+                q.emplace(i, j);
+                vis[i][j] = 1;
+
+                while (!q.empty()) {
+                    auto [r, c] = q.front();
+                    q.pop();
+                    int nr, nc;
+
+                    for (int i = 0; i < 4; i++) {
+                        nr = r + dr[i];
+                        nc = c + dc[i];
+
+                        if (nr >= 0 && nr < n && nc >= 0 
+                        && nc < m 
+                        && !vis[nr][nc]
+                        && grid[nr][nc] == '1') {
+                            q.emplace(nr, nc);
+                            vis[nr][nc] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
