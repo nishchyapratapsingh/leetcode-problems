0ab37560955b36eff7c31d8ef6321aecf83e9351
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-05-14 21:25:28 +0530

    Time: 2 ms (22.74%), Space: 26.4 MB (86.3%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        if(nums[n-1] != n-1) return false;
+        
+        for (int i = 0; i < n; i++) {
+            if (i < n-1 && nums[i] != i+1) return false;
+        }
+
+        return true;
+    }
+};
