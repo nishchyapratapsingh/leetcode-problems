b6b5a8ff80dbedc534a9a2231b93a610a59e58bf
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-05-29 23:27:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.41%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string finalString(string s) {
+        int n = s.length();
+        string ans;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'i') {
+                reverse(ans.begin(), ans.end());
+                continue;
+            }
+
+            ans.push_back(s[i]);
+        }
+        
+        return ans;
+    }
+};
