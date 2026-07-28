6f5fabfedcc52a7cfd2ebf0a55b81722df2c75ed
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-07-28 09:44:43 +0530

    Time: 156 ms (15.6%), Space: 53.9 MB (98.46%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.length();
+        if (n % 2 == 0) {
+          sort(s.begin(), s.begin() + n/2);
+          sort(s.begin() + n/2, s.end(), greater<char>());
+        }
+        else {
+          sort(s.begin(), s.begin() + n/2);
+          sort(s.begin() + n/2 + 1, s.end(), greater<char>());
+        }
+        
+        return s;
+    }
+};
