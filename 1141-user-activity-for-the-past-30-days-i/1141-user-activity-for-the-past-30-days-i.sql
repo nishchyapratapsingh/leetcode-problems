57acc86225ae675f65c614fc88a3e933b8d39cb9
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-08-01 22:39:37 +0530

    Time: 590 ms (18.92%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -2,6 +2,6 @@
 SELECT activity_date AS day,
     COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE DATE(activity_date) >= DATE_SUB('2019-07-27', INTERVAL 29 DAY)
-    AND DATE(activity_date) <= '2019-07-27'
+WHERE activity_date >= DATE_SUB('2019-07-27', INTERVAL 29 DAY)
+    AND activity_date <= '2019-07-27'
 GROUP BY activity_date;
