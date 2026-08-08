91bfbb12447080d7fc1ac2018272741d5c920328
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-08-09 00:26:22 +0530

    Time: 287 ms (60.96%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT (SELECT MAX(salary)
+            FROM Employee
+            WHERE salary != (
+                SELECT MAX(salary) 
+                FROM Employee
+            )
+        )       
+    AS SecondHighestSalary;
