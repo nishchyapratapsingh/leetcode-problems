be8687af5cbf4127615914e1ff7eb8837ed5cc00
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-08-11 17:26:05 +0530

    Time: 319 ms (97.52%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
-SELECT s.id, 
-    CASE 
-        WHEN s.id % 2 = 0 THEN u.student
-        ELSE IFNULL (t.student, s.student)
-    END AS student
+SELECT s.id,
+    IFNULL (t.student, s.student)
+    AS student
 FROM Seat s
 LEFT JOIN Seat t
-ON s.id + 1 = t.id
-LEFT JOIN Seat u
-ON s.id = u.id + 1;
+ON t.id = CASE
+              WHEN s.id % 2 = 0 THEN s.id - 1
+              ELSE s.id + 1
+          END; 
 
