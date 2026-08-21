327bff43b9b90e8eab33cb1f21c3544c2c16e6ba
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-08-22 02:48:06 +0530

    Time: 0 ms (100%), Space: 52.3 MB (31.99%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        int cost = prices[0]+prices[1];
+
+        return cost <= money ? money-cost : money;
+    }
+};
