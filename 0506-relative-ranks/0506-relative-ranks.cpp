812c03d1a33379425d2dc639904b283d920f2581
Nishchya Pratap Singh <149126347+nishchyapratapsingh@users.noreply.github.com>
2026-09-16 02:42:51 +0530

    Time: 4 ms (62.89%), Space: 15.3 MB (86.14%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        priority_queue<pair<int, int>> pq;
+
+        for (int i = 0; i < n; i++) {
+            pq.emplace(score[i], i);
+        }
+
+        vector<string> ans(n);
+
+        int rank = 1;
+
+        while (!pq.empty()) {
+            int idx = pq.top().second;
+            pq.pop();
+
+            if (rank == 1) {
+                ans[idx] = "Gold Medal";
+            }
+            else if (rank == 2) {
+                ans[idx] = "Silver Medal";
+            }
+            else if (rank == 3) {
+                ans[idx] = "Bronze Medal";
+            }
+            else {
+                ans[idx] = to_string(rank);
+            }
+
+            rank++;
+        }
+
+        return ans;
+    }
+};
